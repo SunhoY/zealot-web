0198dcef5f6987a2a9c43aecc3f12f396fb6d2bb
Harry Yang <zillaci@gmail.com>
2017-01-03 10:39:35 +0900

    design menu activity.

--- app/src/main/java/io/harry/zealot/activity/MenuActivity.java
+++ app/src/main/java/io/harry/zealot/activity/MenuActivity.java
@@ -1,5 +1,15 @@
 package io.harry.zealot.activity;
 
-public class MenuActivity extends ZealotBaseActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
+import io.harry.zealot.R;
+
+public class MenuActivity extends ZealotBaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_menu);
+    }
 }
---
+++ app/src/test/java/io/harry/zealot/activity/MenuActivityTest.java
@@ -0,0 +1,13 @@
+package io.harry.zealot.activity;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import io.harry.zealot.BuildConfig;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class MenuActivityTest {
+
+}
