d32ac48da592aad7f1974a20ce37141bb320c9d4
Harry Yang <zillaci@gmail.com>
2017-01-10 15:06:49 +0900

    create ResultActivity to see a result for ajae test and share with others.

---
+++ android/app/src/main/java/io/harry/zealot/activity/ResultActivity.java
@@ -0,0 +1,44 @@
+package io.harry.zealot.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.harry.zealot.R;
+
+public class ResultActivity extends ZealotBaseActivity {
+    private static final String AJAE_SCORE = "ajaeScore";
+
+    private String ajaeScoreText;
+
+    @BindView(R.id.ajae_score)
+    TextView ajaeScore;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_result);
+        ButterKnife.bind(this);
+
+        ajaeScoreText = String.valueOf(getIntent().getIntExtra(AJAE_SCORE, 100));
+        ajaeScore.setText(ajaeScoreText);
+    }
+
+    @OnClick(R.id.test_again)
+    public void onTestAgainClick() {
+        finish();
+    }
+
+    @OnClick(R.id.share_sns)
+    public void onShareSNSClick() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, "http://placeholder.com?score=" + ajaeScoreText);
+
+        startActivity(intent);
+    }
+}
---
+++ android/app/src/test/java/io/harry/zealot/activity/ResultActivityTest.java
@@ -0,0 +1,71 @@
+package io.harry.zealot.activity;
+
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.assertj.android.api.content.IntentAssert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.harry.zealot.BuildConfig;
+import io.harry.zealot.R;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.robolectric.Shadows.shadowOf;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class ResultActivityTest {
+
+    private static final int AJAE_SCORE = 95;
+    private ResultActivity subject;
+
+    @BindView(R.id.ajae_score)
+    TextView ajaeScore;
+
+    @BindView(R.id.test_again)
+    Button testAgain;
+
+    @BindView(R.id.share_sns)
+    Button share;
+
+    @Before
+    public void setUp() throws Exception {
+        Intent intent = new Intent();
+        intent.putExtra("ajaeScore", AJAE_SCORE);
+
+        subject = Robolectric.buildActivity(ResultActivity.class).withIntent(intent).create().get();
+        ButterKnife.bind(this, subject);
+    }
+
+    @Test
+    public void onCreate_setsAjaeScore() throws Exception {
+        assertThat(ajaeScore.getText()).isEqualTo("95");
+    }
+
+    @Test
+    public void clickOnTestAgain_finishesActivity() throws Exception {
+        testAgain.performClick();
+
+        assertThat(subject.isFinishing()).isTrue();
+    }
+
+    @Test
+    public void clickOnShare_sendShareIntentWithLink() throws Exception {
+        share.performClick();
+
+        Intent actual = shadowOf(subject).getNextStartedActivity();
+
+        IntentAssert intentAssert = new IntentAssert(actual);
+
+        assertThat(intentAssert.hasAction(Intent.ACTION_SEND));
+        assertThat(intentAssert.hasExtra(Intent.EXTRA_TEXT, "http://placeholder.com?score=95"));
+    }
+}
